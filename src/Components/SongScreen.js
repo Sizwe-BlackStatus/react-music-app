51741bddd39ed9cec4d3856b9a47b7255be61562
Sizwe <sizwemasemola1@gmail.com>
2021-09-16 13:12:52 +0200

    fixed position of songScreen component

--- src/Components/SongScreen.js
+++ src/Components/SongScreen.js
@@ -2,13 +2,14 @@ import styled from "styled-components";
 
 export const SongScreenContainer = styled.div`
   background: transparent;
-  top: 480px;
+  top: 50px;
   left: ${({ open }) => (open ? "-208px" : "0px")};
   height: 250px;
   display: ${({music}) => (typeof music === 'object' ? 'block' : 'none')};
   width: 196px;
-  position: fixed;
+  position: relative;
   z-index: 999;
+  margin-top: 10px;
   @media screen and (max-width: 560px) {
     flex: 0.1;
     align-items: center;
